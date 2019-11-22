c4cea06d389ca98774817003c922103243f656dc
steven oakley <stevenoakley211@live.com>
2019-11-21 21:22:30 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import React, {useEffect,useState} from "react";
 import "./App.css";
 import axios from "axios";
 import ImageCard from "./Components/ImageCard"
+import styled from "styled-components"
 function App() {
 
   const [images,setImage] = useState('')
   const [title,setTitle] = useState('')
+  const [description,setDescription]= useState('')
+
+  const FlexDiv = styled.div `
+    display:flex;
+    justify-content:center;
+    background-color:#002366 ;
+  `
   const didUpdate = () => {
     axios
       .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(response => {
         setImage(response.data.hdurl)
         setTitle(response.data.title)
+        setDescription(response.data.explanation)
       })
       .catch(error => console.log(error))
   }
   useEffect(didUpdate,[])
   console.log(images)
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    <ImageCard title={title} imgUrl={images}/>
-    </div>
+    <FlexDiv className="App">
+    <ImageCard title={title} imgUrl={images} content ={description}/>
+    </FlexDiv>
   );
 }
 
--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,11 +1,34 @@
 import React from "react";
-
+import styled from "styled-components"
 const ImageCard = props => {
+    const StyledH1 = styled.h1 `
+    text-decoration: underline overline #002366;
+    
+    `
+    const Card = styled.div`
+    background-color:white;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.5);
+    transition: 0.3s;
+    border-radius: 5px;
+    width:50%;
+    padding:20px;
+    margin: 20px;
+    font-family:'Helvetica';
+    `
+    const Image = styled.img `
+        max-width:100%;
+        border-radius: 5px;
+    `
+    const Paragraph = styled.p `
+        font-size:1.25em;
+    `
     return(
-        <div>
-            <h2>{props.title}</h2>
-            <img className="img"src={props.imgUrl}></img>
-        </div>
+        <Card>
+            <StyledH1>{props.title}</StyledH1>
+            <Image src={props.imgUrl}></Image>
+            <h2>Description</h2>
+            <Paragraph>{props.content}</Paragraph>
+        </Card>
     )
 }
 export default ImageCard
